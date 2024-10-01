d8a71ae62f5f13255f6a66e57b913a890587bf36
20essentials <s0l0estudi4r@gmail.com>
2024-10-01 18:45:32 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,10 @@
+console.group("Reference");
+console.log(
+  "Metal texture",
+  "https://i.pinimg.com/736x/27/29/28/272928d318699b8600545efbfce78481.jpg"
+);
+console.log(
+  "Manzdev Video",
+  "https://youtu.be/jSia5SigafE?si=RRFrWX39S6Xwm9Br"
+)
+console.groupEnd();
